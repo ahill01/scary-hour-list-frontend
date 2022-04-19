7ee44f11062381825086d28cc0fa114b6b08f1d2
Allison <amh03160@gmail.com>
2022-04-19 15:51:12 -0500

    timer in progress

--- scary-hour-list-frontend/src/components/TaskListItem.js
+++ scary-hour-list-frontend/src/components/TaskListItem.js
@@ -5,7 +5,6 @@ function TaskListItem({id, name, estimatedTime, scariness}){
         <div className="taskitem">
         <h2>TASK: {name}</h2>
         <h2>ESTIMATED TIME: {estimatedTime} minutes</h2>
-        <h2></h2>
         <h2>scariness: {scariness}</h2>
         </div>
     )
--- scary-hour-list-frontend/src/components/Timer.js
+++ scary-hour-list-frontend/src/components/Timer.js
@@ -1,7 +1,30 @@
-import React from "react"
-function Timer(){
+import React, {useState} from "react"
 
-    return
+function Timer(){
+const[paused, setPaused]=useState(false) 
+let num = 0;
+
+//counter
+// function count(){
+//     if(!paused){
+//     num++;
+//     console.log(num)
+//     return num;
+// };
+// }
+useEffect(() => {
+  const timer = setTimeout(() => {
+    setTimeLeft(calculateTimeLeft());
+  }, 1000);
+});
+
+// setInterval(count,1000);
+
+    return(
+        <div className="counter">
+           <button onCLick={setPaused(paused => !paused)}>{paused ? "paused" : `${num}`}</button>
+        </div>
+    )
 }
 
 export default Timer
