a132443be183aa1a98ad67b47d831a72edcf9798
Allison <amh03160@gmail.com>
2022-04-21 09:50:49 -0500

    added login feature

--- scary-hour-list-frontend/src/components/LogInWindow.js
+++ scary-hour-list-frontend/src/components/LogInWindow.js
@@ -2,22 +2,29 @@ import React, {useState, useEffect} from "react"
 import UserButton from "./UserButton";
 
 function LogInWindow({users, setCurrentUser}){
-
-    function handleChange(e){
-        
+const [username, setUsername] = useState("")
+    
+function handleChange(e){
+        console.log("calling handleChange")
+       setUsername(e.target.value)
     }
+
     function handleLogIn(e){
         e.preventDefault()
-
+        const foundUser = users.find(user => user.name===username)
+        console.log(foundUser)
+        if(foundUser) {
+            setCurrentUser(foundUser)
+        } else {
+            window.alert("Account not found. Please create an account to login")
+        }
     }
 
     return(
     <div className="login">
-        {/* {users.map(user => <UserButton username={user.name} userId = {user.id} setCurrentUser={setCurrentUser}></UserButton>)} */}
-
         <form>
-            <input type="text" placeholder="username"></input>
-            <button type="submit">Log Me In!</button>
+            <input type="text" placeholder="username" onChange={handleChange}></input>
+            <button type="submit" onClick={handleLogIn}>Log Me In!</button>
         </form>
     </div>
     )
