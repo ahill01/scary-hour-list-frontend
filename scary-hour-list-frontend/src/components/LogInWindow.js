87d262bbc7cc885dc3de9999fbdd879c09d86e42
Ethan Lee <wxliyc@gmail.com>
2022-04-21 13:41:25 -0500

    ethan-day4-css1

--- scary-hour-list-frontend/src/components/LogInWindow.js
+++ scary-hour-list-frontend/src/components/LogInWindow.js
@@ -1,34 +1,41 @@
-import React, {useState, useEffect} from "react"
-import NewUserForm from "./NewUserForm"
-function LogInWindow({users, setCurrentUser}){
-const [username, setUsername] = useState("")
-    
-function handleChange(e){
-        console.log("calling handleChange")
-       setUsername(e.target.value)
-    }
+import React, { useState, useEffect } from "react";
+import NewUserForm from "./NewUserForm";
+function LogInWindow({ users, setCurrentUser }) {
+  const [username, setUsername] = useState("");
 
-    function handleLogIn(e){
-        e.preventDefault()
-        const foundUser = users.find(user => user.name===username)
-        console.log(foundUser)
-        if(foundUser) {
-            setCurrentUser(foundUser)
-        } else {
-            window.alert("Account not found. Please create an account to login")
-        }
-    }
+  function handleChange(e) {
+    console.log("calling handleChange");
+    setUsername(e.target.value);
+  }
 
-    return(
+  function handleLogIn(e) {
+    e.preventDefault();
+    const foundUser = users.find((user) => user.name === username);
+    console.log(foundUser);
+    if (foundUser) {
+      setCurrentUser(foundUser);
+    } else {
+      window.alert("Account not found. Please create an account to login");
+    }
+  }
+
+  return (
     <div className="login">
-        <form>
-            <h2>Log In</h2>
-            <input className="NewContent" type="text" placeholder="username" onChange={handleChange}></input>
-            <button className="NewContent" type="submit" onClick={handleLogIn}>Log Me In!</button>
-        </form>
-        <NewUserForm/>
+      <form>
+        <h2>Log In</h2>
+        <input
+          className="NewContent"
+          type="text"
+          placeholder="username"
+          onChange={handleChange}
+        ></input>
+        <button className="NewContent" type="submit" onClick={handleLogIn}>
+          Log Me In!
+        </button>
+      </form>
+      <NewUserForm />
     </div>
-    )
+  );
 }
 
-export default LogInWindow
+export default LogInWindow;
