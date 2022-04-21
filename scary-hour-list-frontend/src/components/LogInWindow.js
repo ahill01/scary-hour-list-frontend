a5fd8164f44aa82e29b078a26e7e1b34c9176dab
Allison <amh03160@gmail.com>
2022-04-21 09:41:19 -0500

    updating login window

--- scary-hour-list-frontend/src/components/LogInWindow.js
+++ scary-hour-list-frontend/src/components/LogInWindow.js
@@ -3,9 +3,22 @@ import UserButton from "./UserButton";
 
 function LogInWindow({users, setCurrentUser}){
 
+    function handleChange(e){
+        
+    }
+    function handleLogIn(e){
+        e.preventDefault()
+
+    }
+
     return(
     <div className="login">
-        {users.map(user => <UserButton username={user.name} userId = {user.id} setCurrentUser={setCurrentUser}></UserButton>)}
+        {/* {users.map(user => <UserButton username={user.name} userId = {user.id} setCurrentUser={setCurrentUser}></UserButton>)} */}
+
+        <form>
+            <input type="text" placeholder="username"></input>
+            <button type="submit">Log Me In!</button>
+        </form>
     </div>
     )
 }
